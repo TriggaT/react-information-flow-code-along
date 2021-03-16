8c2458a7911d94d5bb6f4e7d863bbc52640ba3d1
Trayshawn <traytwebb14@yahoo.com>
2021-03-16 17:52:49 -0500

    commit tree 5e66b82f0ad861c03d7da531740f7c926e663a5f

--- src/Parent.js
+++ src/Parent.js
@@ -4,18 +4,25 @@ import Child from './Child'
 
 class Parent extends Component {
 
-  constructor() {
+  constructor(newChildColor) {
     super()
     this.state = {
-      color: getRandomColor()
+      color: getRandomColor(),
+      childrenColor: newChildColor
     }
+  }
+
+  changeColor = () => {
+    this.setState({
+      color: getRandomColor()
+    })
   }
 
   render() {
     return (
       <div className="parent" style={{backgroundColor: this.state.color}}>
-        <Child />
-        <Child />
+          <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
+          <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
       </div>
     )
   }
